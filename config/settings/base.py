610d39e1f94f711c1d8299794da494288a39788e
Michael Assanama <assanamamichael@gmail.com>
2025-01-06 16:32:51 +0100

    update list

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,7 +21,7 @@ environ.Env.read_env()  # Reads the .env file
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = True
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
